cf17132a5dcff2ea34cfb1f82c7397638a8dd9b3
Brett Hobbs <46791301+BrettTheHobbit@users.noreply.github.com>
2023-02-17 13:28:05 -0500

    Added scene change script

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -5,11 +5,16 @@ using UnityEngine.SceneManagement;  // Import the SceneManager class
 
 public class SceneChange : MonoBehaviour
 {
-
     public string scene;
     // Start is called before the first frame update
     void SceneChanger()
     {
         SceneManager.LoadScene(scene); // Loads the scene with the requested name
+    }
+
+    void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();//Quits the game
     }
 }
